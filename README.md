efaf03556c6b5c582ad77a302d95834861f2bdac
Randy Kreisel <randy@rkreisel.com>
2023-12-14 16:17:52 -0600

    Updated Readme and added XML comments.

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Text viewer is a simple dll in a Nuget package that will open a popup window with text and a simple search feature.
 
+Search history is maintained for the most recent 100 searches for each application. The developer may optionally specify a name to the search history file in order to provide different history records for different purposes.
+
 ## Project
 
 https://github.com/rkreisel/TextViewer.git
@@ -14,10 +16,12 @@ NuGet\Install-Package TextViewer - Version 1.0.0
 
 ## Usage
 
+**Note**  In both examples, the second parameter is optional. If not specified all viewer instances will share a common file. It will reside in the ProgramData folder under \HotRS\TextViewer\\*appname*\SearchHist-TxtVwrSrchHst.txt
+
 ### Load text that you create in code:
 
 ```c#
-var viewer = new Viewer.Viewer("Viewer1");
+var viewer = new TextViewer.Viewer("Viewer1", "userdefinednameforsearchhistoryfile");
 viewer.LoadText("This is some text");
 viewer.Show();
 ```
@@ -25,7 +29,7 @@ viewer.Show();
 ### Load text from file
 
 ```c#
-var viewer = new Viewer.Viewer("Viewer2");
+var viewer = new TextViewer.Viewer("Viewer2", "userdefinednameforsearchhistoryfile");
 viewer.LoadFile(validfilename);
 viewer.Show();
 ```
--- TextViewer/Utilities/Utils.cs
+++ TextViewer/Utilities/Utils.cs
@@ -1,10 +1,16 @@
-﻿using System.Text;
-using System.Text.RegularExpressions;
+﻿namespace TextViewer.Utilities;
 
-namespace TextViewer.Utilities;
-
+/// <summary>
+/// A class containing miscellaneous extension methods.
+/// </summary>
 public static class Utils
 {
+    /// <summary>
+    /// Checks an object for being null. Note that this can be replaced with ArgumentNUllExpcetipn.ThrowIfNull
+    /// </summary>
+    /// <param name="o">The object.</param>
+    /// <param name="paramName">The Name of the object.</param>
+    /// <exception cref="ArgumentNullException"></exception>
     public static void CheckForNull(this object o, string paramName)
     {
         if (o == null)
@@ -13,15 +19,32 @@ public static class Utils
         }
     }
 
+    /// <summary>
+    /// Evaluates a string as being an valid integer.
+    /// </summary>
+    /// <param name="source">A string</param>
+    /// <returns>True or False</returns>
     public static bool IsNumeric(this string source) =>
         int.TryParse(source, out _);
 
+    /// <summary>
+    /// Uses Reflection to determine if an object contains the given property.
+    /// </summary>
+    /// <param name="obj">An instance of an object.</param>
+    /// <param name="propertyName">The name of the property to find.</param>
+    /// <returns>True or False</returns>
     public static bool HasProperty(this object obj, string propertyName)
     {
         obj.CheckForNull(nameof(obj));
         return obj.GetType().GetProperty(propertyName) != null;
     }
 
+    /// <summary>
+    /// Add the specified character to the end of a string if it is not already there.
+    /// </summary>
+    /// <param name="source">A string</param>
+    /// <param name="finalChar">The character to evaluate.</param>
+    /// <returns>A string with one instance of the specified character.</returns>
     public static string EnsureFinalChar(this string source, char finalChar)
     {
         if (source.EndsWith(finalChar))
@@ -30,6 +53,13 @@ public static class Utils
             return $"{source}{finalChar}";
 
     }
+
+    /// <summary>
+    /// Removes the specified character from the string if it is teh last character.
+    /// </summary>
+    /// <param name="source">A string</param>
+    /// <param name="finalChar">The character to remoove.</param>
+    /// <returns>A string where the last character is NOT athe speficied one.</returns>
     public static string EnsureDoesNotEndWith(this string source, char finalChar)
     {
         if (source.EndsWith(finalChar))
@@ -39,17 +69,13 @@ public static class Utils
 
     }
 
+    /// <summary>
+    /// Eliminates all special characters from a string. This is useful when a 'safe' string is needed. For instance to build a filename.
+    /// </summary>
+    /// <param name="str">A string</param>
+    /// <returns>A string with only alphanumeric characters.</returns>
     public static string RemoveSpecialCharacters(this string str)
     {
         return Regex.Replace(str, "[^a-zA-Z0-9_.]+", "", RegexOptions.Compiled);
-        //StringBuilder sb = new StringBuilder();
-        //foreach (char c in str)
-        //{
-        //    if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '.' || c == '_')
-        //    {
-        //        sb.Append(c);
-        //    }
-        //}
-        //return sb.ToString();
     }
 }
